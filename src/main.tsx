496cf7f581dbfd7b604e8cae404954586088654d
Sollee <solleedev@proton.me>
2023-07-04 14:36:44 +0100

    yes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'tailwindcss/tailwind.css';
 
-import App from './App';
+import App from './routes/App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   );
 }
